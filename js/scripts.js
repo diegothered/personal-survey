d8593371ce23a55c98870fa21996040f00919db4
Diego Rojas and Jaime Takikawa <diego@>
2019-05-29 20:45:20 -0700

    add different forms to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   $("#fav-things-form").submit(function(event) {
     var color = $("input#color").val();
-    var food = $("input#food").val();
-    var animal = $("input#animal").val();
+    var food = $("input:radio[name=food]:checked").val();
 
     $(".color").text(color);
     $(".food").text(food);
-    $(".animal").text(animal);
+
 
     $("#your-favs").show();
-
     event.preventDefault();
+  })
+  $()
 
-  });
+
+
+
+
+
+
 });
